d28cbc66eda20333fd3e9c01edc0469d20eeb4bd
unknown <anukulpk2@gmail.com>
2022-08-04 12:45:52 +0530

    Added get API for todos

--- src/main/java/com/example/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.example.restfulwebservices;
+package com.example.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +21,7 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+//        throw new RuntimeException("Something went wrong");
         return new HelloWorldBean( String.format("Hello World %s", name));
     }
 }
---
+++ src/main/java/com/example/restfulwebservices/todo/Todo.java
@@ -0,0 +1,59 @@
+package com.example.restfulwebservices.todo;
+
+import java.util.Date;
+
+public class Todo {
+    private long id;
+    private String usernmae;
+    private String description;
+    private Date targetDate;
+    private boolean isDone;
+
+    public Todo(long id, String usernmae, String description, Date targetDate, boolean isDone) {
+        this.id = id;
+        this.usernmae = usernmae;
+        this.description = description;
+        this.targetDate = targetDate;
+        this.isDone = isDone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsernmae() {
+        return usernmae;
+    }
+
+    public void setUsernmae(String usernmae) {
+        this.usernmae = usernmae;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getTargetDate() {
+        return targetDate;
+    }
+
+    public void setTargetDate(Date targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
